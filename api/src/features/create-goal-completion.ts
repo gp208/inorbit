21988ea21ba55ef1f8a0f7873e7185a71fbdc587
gp208 <gustavocprieto@gmail.com>
2025-11-30 12:46:40 -0300

    add goal completion creation function and API route

---
+++ api/src/features/create-goal-completion.ts
@@ -0,0 +1,45 @@
+import dayjs from 'dayjs'
+import { and, count, eq, gte, lte, sql } from 'drizzle-orm'
+import { db } from '../db'
+import { goalCompletions, goals } from '../db/schema'
+
+interface CreateGoalCompletionRequest {goalId: string}
+
+export async function createGoalCompletion({goalId}: CreateGoalCompletionRequest) {
+  const firstWeekDay = dayjs().startOf('week').toDate()
+  const lastWeekDay = dayjs().endOf('week').toDate()
+
+  const goalCompletionCounts = db.$with('goal_completion_counts').as(db
+    .select({
+      goalId: goalCompletions.goalId,
+      completionCount: count(goalCompletions.id).as('completionCount')
+    })
+    .from(goalCompletions)
+    .where(and(
+      gte(goalCompletions.createdAt, firstWeekDay),
+      lte(goalCompletions.createdAt, lastWeekDay),
+      eq(goalCompletions.goalId, goalId)
+      )
+    )
+    .groupBy(goalCompletions.goalId)
+  )
+
+  const result = await db
+    .with(goalCompletionCounts)
+    .select({
+      desiredWeeklyFrequency: goals.desiredWeeklyFrequency,
+      completionCount: sql`COALESCE(${goalCompletionCounts.completionCount}, 0)`.mapWith(Number)
+    })
+    .from(goals)
+    .leftJoin(goalCompletionCounts, eq(goalCompletionCounts.goalId, goals.id))
+    .where(eq(goals.id, goalId))
+    .limit(1)
+  
+  const {completionCount, desiredWeeklyFrequency} = result[0]
+  if(completionCount >= desiredWeeklyFrequency)
+    throw new Error('This goal has already been completed this week')
+
+  const insertResult = await db.insert(goalCompletions).values({goalId}).returning()
+  const goalCompletion = insertResult[0]
+  return {goalCompletion}
+}
