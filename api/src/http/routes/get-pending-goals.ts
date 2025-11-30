73a6bffc47d32556121715c6a5bd1225f01500ef
gp208 <gustavocprieto@gmail.com>
2025-11-30 12:51:14 -0300

    put route definitions in separate files

---
+++ api/src/http/routes/get-pending-goals.ts
@@ -0,0 +1,12 @@
+import { FastifyPluginAsyncZod } from 'fastify-type-provider-zod'
+import { getWeekPendingGoals } from '../../features/get-week-pending-goals'
+
+export const getPendingGoalsRoute: FastifyPluginAsyncZod = async (app) => {
+  app.get(
+    '/pending-goals',
+    async () => {
+      const {pendingGoals} = await getWeekPendingGoals()
+      return {pendingGoals}
+    }
+  )
+}
