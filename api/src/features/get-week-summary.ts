a23ce9f74085bc9ac26927d4ebfb0cb08103ad87
gp208 <gustavocprieto@gmail.com>
2025-11-30 16:12:37 -0300

    fetch and register goal completions on button press

--- api/src/features/get-week-summary.ts
+++ api/src/features/get-week-summary.ts
@@ -1,5 +1,5 @@
 import dayjs from 'dayjs'
-import { and, eq, gte, lte, sql } from 'drizzle-orm'
+import { and, desc, eq, gte, lte, sql } from 'drizzle-orm'
 import { db } from '../db'
 import { goalCompletions, goals } from '../db/schema'
 
@@ -44,6 +44,7 @@ export async function getWeekSummary() {
     })
     .from(goalsCompletedInWeek)
     .groupBy(goalsCompletedInWeek.completedAtDate)
+    .orderBy(desc(goalsCompletedInWeek.completedAtDate))
   )
 
   type GoalsPerDay = Record<string, {id: string, title: string, completedAt: string}[]>
--- web/src/components/pending-goals.tsx
+++ web/src/components/pending-goals.tsx
@@ -1,17 +1,29 @@
 import { useQuery } from '@tanstack/react-query'
 import { Plus } from 'lucide-react'
+import { createGoalCompletion } from '../http/create-goal-completion'
 import { getPendingGoals } from '../http/get-pending-goals'
 import { OutlineButton } from './ui/outline-button'
 
 export function PendingGoals() {
-  const {data} = useQuery({queryKey: ['pending-goals'], queryFn: getPendingGoals})
+  const {data} = useQuery({
+    queryKey: ['pending-goals'],
+    queryFn: getPendingGoals
+  })
   if(!data) return null
+
+  async function handleCompletedGoal(goalId: string) {
+    await createGoalCompletion(goalId)
+  }
 
   return (
     <div className='flex flex-wrap gap-3'>
       {data.map(goal => {
         return(
-          <OutlineButton key={goal.id} disabled={goal.completionCount >= goal.desiredWeeklyFrequency}>
+          <OutlineButton
+            key={goal.id}
+            disabled={goal.completionCount >= goal.desiredWeeklyFrequency}
+            onClick={() => handleCompletedGoal(goal.id)}
+          >
             <Plus className='size-4 text-zinc-600'/>{goal.title}
           </OutlineButton>)
       })}
